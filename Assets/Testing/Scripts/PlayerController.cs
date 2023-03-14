c9eed86e86fb2ac65b507285afb0de259223d8e4
gigadev <gigadevteam666@gmail.com>
2023-03-14 20:34:35 +0300

    added playercontroller v0

---
+++ Assets/Testing/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    
+
+    private Rigidbody playerRigidbody;
+    private PlayerInput playerInput;
+
+    private PlayerControls playerControls;
+
+    private void Awake() {
+        playerRigidbody = GetComponent<Rigidbody>();
+        playerInput = GetComponent<PlayerInput>();
+        
+        playerControls = new PlayerControls();
+        
+        playerControls.Player.Enable();
+        playerControls.Player.Jump.performed += Jump;
+        
+    }
+    
+    private void FixedUpdate() {
+
+        Move(playerControls.Player.Move.ReadValue<Vector2>());
+
+    }
+    
+    public void Move(Vector2 inputValue){
+        inputValue *= 10f;
+        playerRigidbody.velocity = new Vector3(inputValue.x, playerRigidbody.velocity.y, inputValue.y);
+    }
+    
+    public void Jump(InputAction.CallbackContext context){
+        if (!context.performed) return;
+
+        playerRigidbody.AddForce(Vector3.up * 10f, ForceMode.Impulse);
+    }
+    
+}
